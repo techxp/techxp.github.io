458461a7a108f364c5bc9b12bb12a9ac7a204ffb
santosh <santosh.ece06@gmail.com>
2018-10-04 22:37:26 +0530

    routing added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Router, RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from 'src/app/page1/page1.component';
+import { Page2Component } from 'src/app/page2/page2.component';
+
+const appRoutes: Routes = [
+  {'path': 'page1', 'component' : Page1Component},
+  {'path': 'page2', 'component' : Page2Component},
+  {path: '', redirectTo: '/page1', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
